83784d0f30833403dd71dfe291254bf2d8d245ba
adynd <adynd@DESKTOP-KJ7Q87O>
2023-04-05 20:16:25 +0800

    Add project files.

---
+++ Vidly_Mosh_KZK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Mosh_KZK.Startup))]
+namespace Vidly_Mosh_KZK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
